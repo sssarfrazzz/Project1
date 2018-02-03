5d7b1d68f3dadf3c7c7515f489730622f5e54e39
sssarfrazzz <sarfrazshaikh14@gmail.com>
2018-02-03 20:05:25 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { PracticeAreasComponent } from './practice-areas/practice-areas.component';
 const routes: Routes = [{
   path: '',
   component: DashboardComponent,
+    children : [
+      // { path: '', redirectTo: 'practice-areas', pathMatch: 'full'},
+      { path: '', component: PracticeAreasComponent},
+      { path: 'about-us', component: AboutusComponent},
+    ]
 },
 ];
 
